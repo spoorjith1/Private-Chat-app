924a1cac1c380a251a0a70102deb37230a14f509
spoorjith1 <spoorjith1@gmail.com>
2026-05-16 14:01:02 +0530

    backend api's

--- backend-drf/api/urls.py
+++ backend-drf/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework_simplejwt import views as JwtViews
 from accounts import views as AccViews
+from friendships import views as FrdShipsViews
+
 
 urlpatterns = [
     #register
@@ -8,5 +10,15 @@ urlpatterns = [
     
     #login
     path('token/', JwtViews.TokenObtainPairView.as_view(), name='obtain_token'),
-    path('token/refresh', JwtViews.TokenRefreshView.as_view(), name='refresh_token'),
+    path('token/refresh/', JwtViews.TokenRefreshView.as_view(), name='refresh_token'),
+    
+    #friendships
+    #friend request
+    path('friend/request/', FrdShipsViews.FriendRequestView.as_view(), name=''),
+    #pending requests
+    path('friend/requests/', FrdShipsViews.PendingRequestView.as_view(), name=''),
+    #request response
+    path('friend/response/<int:pk>/', FrdShipsViews.RespondFriendRequestView.as_view(), name=''),
+    #friends list
+    path('friend/list/', FrdShipsViews.FriendsListView.as_view(), name=''),
 ]
